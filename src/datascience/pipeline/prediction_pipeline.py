496f37e1b2a34efdd254cf509b06b63cfbaf91b1
adi2075 <adigit2075.14111998@gmail.com>
2024-12-20 13:09:20 +0100

    flask app.py added with html

---
+++ src/datascience/pipeline/prediction_pipeline.py
@@ -0,0 +1,15 @@
+# load the model and do the prediction
+
+import joblib
+import numpy as np  
+import pandas as pd
+from pathlib import Path
+
+class PredictionPipeline:
+    def __init__(self):
+        self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+        
+    def predict(self, data):
+        prediction=self.model.predict(data)
+        
+        return prediction
